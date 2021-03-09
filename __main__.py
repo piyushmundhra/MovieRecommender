413b14e1886fb98f1aefa8c2e91754f38146bdfe
Piyush Mundhra <piyushmundhra@Piyushs-MacBook-Pro.local>
2021-03-08 17:54:55 -0800

    Fixed constructor usage

--- __main__.py
+++ __main__.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 from rpkg.recommender import Recommender
 
-r = Recommender(pd.Series([1]))
+r = Recommender()
 print(r.getRecommendation(r.imdbToMovie('0372784')))
