893b4005a0c8f6966ffa989cfb625d673f64efd1
jhan136 <jhan136@ucr.edu>
2021-03-08 20:20:55 -0800

    made options1.py

---
+++ optpkg/options1.py
@@ -0,0 +1,16 @@
+import sys
+import pandas
+from optpkg.userOptions import userOptions
+from optpkg.rpkg.recommender import Recommender
+
+class Options1(userOptions):
+	#
+	r = Recommender()
+	def getUserOptions(self):
+		#no options
+	def getUserTaste(self):
+		#somehow limit movies being looked at 
+		print("")
+		if(_integer(placeholder))
+	def giveUserRecs(self):
+	def approveRecommendation():
